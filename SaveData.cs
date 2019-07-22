e821a332dde277309eef39ce78a88711b699aed5
Ethan Vong <32001455+Hambonaire@users.noreply.github.com>
2019-07-22 17:01:27 -0500

    Transfer scripts thru GitHub
    saving and loading stuff

---
+++ SaveData.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// Responsible for:
+/// - Maintaining the stats for a player and their progress
+/// - Writing this data to a file.
+/// - Reading this data from a file.
+/// </summary>
+public class SaveData
+{
+    #region Defaults
+    private const int DEFAULT_MONEY = 0;
+    public const int DEFAULT_COCKPIT_ID = 0;
+    public const int DEFAULT_WEAPON_LEFT_ID = 0;
+    public const int DEFAULT_WEAPON_RIGHT_ID = 0;
+    public const int DEFAULT_LEGS_ID = 0;
+    public const int DEFAULT_LEVEL = 0;
+    #endregion
+
+    public int cockpit = DEFAULT_COCKPIT_ID;
+    public int weaponLeft = DEFAULT_WEAPON_LEFT_ID;
+    public int weaponRight = DEFAULT_WEAPON_RIGHT_ID;
+    public int legs = DEFAULT_LEGS_ID;
+    public int lastLevel = DEFAULT_LEVEL;
+    public int money = DEFAULT_MONEY;
+    public List<int> accessories = new List<int>();
+    public List<int> inventoryItems = new List<int>();
+
+    public void SaveData()
+    {
+        string json = JsonUtility.ToJson(this, true);
+
+        int key = 129;
+
+        StringBuilder inSb = new StringBuilder(json);
+        StringBuilder outSb = new StringBuilder(json.Length);
+        char c;
+        for (int i = 0; i < json.Length; i++)
+        {
+            c = inSb[i];
+
+            c = (char)(c ^ key);
+            outSb.Append(c);
+        }
+
+        File.WriteAllText(Application.persistentDataPath + "/Saves.json", outSb.ToString());
+    }
+
+    public static SaveData LoadData()
+    {
+        if (!File.Exists(Application.persistentDataPath + "/Saves.json"))
+        {
+            Debug.Log("File not found, returning new object");
+            return new SaveData();
+        }
+        else
+        {
+            string contents = File.ReadAllText(filePath);
+
+            if (string.IsNullOrEmpty(contents))
+            {
+                Debug.Log("File is empty. Returning default SaveData");
+                return new SaveData();
+            }
+
+            int key = 129;
+
+            StringBuilder inSb = new StringBuilder(contents);
+            StringBuilder outSb = new StringBuilder(contents.Length);
+            char c;
+            for (int i = 0; i < contents.Length; i++)
+            {
+                c = inSb[i];
+                c = (char)(c ^ key);
+                outSb.Append(c);
+            }
+
+            return JsonUtility.FromJson<SaveData>(outSb.ToString());
+        }
+    }
+
+    public bool IsDefault()
+    {
+        return (
+            money == DEFAULT_MONEY &&
+            cockpit == DEFAULT_COCKPIT_ID &&
+            weaponLeft == DEFAULT_WEAPON_LEFT_ID &&
+            weaponRight == DEFAULT_WEAPON_RIGHT_ID &&
+            legs == DEFAULT_LEGS_ID &&
+            lastLevel == DEFAULT_LEVEL &&
+            accessories.Count == 0 && 
+            inventoryItems.Count == 0);
+    }
+
+}
