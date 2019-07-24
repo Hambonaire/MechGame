db467e82367f2f3046d54b268d3a2cee56d24a55
Ethan Vong <32001455+Hambonaire@users.noreply.github.com>
2019-07-24 16:57:22 -0500

    Update Cockpit.cs

--- MechGameUnity/Assets/Scripts/Items/Cockpit.cs
+++ MechGameUnity/Assets/Scripts/Items/Cockpit.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Cockpit", menuName = "Mecha/Cockpit")]
 public class Cockpit : Item {
 
+    public int rightRegularCount = 1;
+    public int rightUnderhandCount = 0;
+    public int rightShoulderCount = 0;
+    
+    public int leftRegularCount = 1;
+    public int leftUnderhandCount = 0;
+    public int leftShoulderCount = 0;
+    
+    public int accessoryCount = 0;
+    
     public GameObject prefab;
     public float ballisticArmor;
     public float energyArmor;
