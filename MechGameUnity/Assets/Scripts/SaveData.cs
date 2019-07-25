df8a85928023e5754ada191cf1f5ec621f50ee22
Ethan Vong <32001455+Hambonaire@users.noreply.github.com>
2019-07-25 17:02:46 -0500

    Update SaveData.cs

--- MechGameUnity/Assets/Scripts/SaveData.cs
+++ MechGameUnity/Assets/Scripts/SaveData.cs
@@ -18,8 +18,9 @@ public class SaveData
     public int cockpit = DEFAULT_COCKPIT_ID ;
     //private int weaponLeft { get; set; } = DEFAULT_WEAPON_LEFT_ID;
     //private int weaponRight { get; set; } = DEFAULT_WEAPON_RIGHT_ID;
-    public List<int> leftWeapons = new List<int>();
-    public List<int> rightWeapons = new List<int>();
+    //public List<int> leftWeapons = new List<int>();
+    //public List<int> rightWeapons = new List<int>();
+    public List<int> weapons = new List<int>();
     public int legs = DEFAULT_LEGS_ID;
     public int lastLevel = DEFAULT_LEVEL;
     public int money = DEFAULT_MONEY;
