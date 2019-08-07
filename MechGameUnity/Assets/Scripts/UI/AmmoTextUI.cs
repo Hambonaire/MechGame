4a265555fc1d7950f9ef1666081ca5f2218b89c0
Ethan Vong <32001455+Hambonaire@users.noreply.github.com>
2019-08-07 17:20:28 -0500

    Update AmmoTextUI.cs

--- MechGameUnity/Assets/Scripts/UI/AmmoTextUI.cs
+++ MechGameUnity/Assets/Scripts/UI/AmmoTextUI.cs
@@ -6,6 +6,36 @@ using UnityEngine.UI;
 
 public class AmmoTextUI : MonoBehaviour
 {
+    public Datatype_Weapon data_ref;
+    public Image fillBar;
+    public Image reloadImage;
+    public Text text;
+    public int current;
+    public int max;
     
+    void Start () 
+    {
+    
+    }
+    
+    void LateUpdate ()
+    {
+        current = data_ref.currentAmmo;
+        
+        fillBar.fillAmount = current/max;
+        
+        if (current == 0 || data_ref.isReloading) {
+            reloadImage.enabled = true;
+            // TODO: Do more stuff here, make it spin or something
+        }
+        
+        current
+    }
+    
+    public void Rebuild () {
+        max = data_ref.maxAmmo;
+        current = max;
+        
+    }
 }
  
